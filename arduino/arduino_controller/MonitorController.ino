f5bc63f062f13e343b69b63437f87b23b895ad9d
NothingFace <nothingface@Hackintosh.local>
2021-04-03 23:09:52 -0700

    version 2 added
    Now uses AccelStepper library for acceleration

--- arduino/arduino_controller/MonitorController.ino
+++ arduino/arduino_controller/MonitorController.ino
@@ -1,31 +1,37 @@
 /*
- * MONITOR CONTROLLER V1
- 
-   Controls the orientation of a monitor using an DRV8825 or A4988 and a NEMA 17 stepper.
+   MONITOR CONTROLLER V2
+
+   Now uses AccelStepper library for acceleration
+
+   Controls the orientation of a monitor using an DRV8825 or A4988 and a NEMA 17 stepper motor.
    Uses 2 limit switches to stop rotation at vertical & horizontal positions.
-   Also utilizes a momentary switch to manually switch orientatians.
+   Also utilizes a momentary switch to manually switch orientatians if necessary.
 
    ***********************
    CONNECTIONS:
 
    NEMA 17 Coils = Black/Green, Red/Blue
 
+   ** FOR A4988 ONLY **
    To stop motor chatter on startup/reset, place a 10k pulldown resistor on
    the step pin (step pin to Arduino, plus step pin to 10k then to ground).
+   (only needed for A4988 controller)
 
    All switches connect to ground and their respeccted pins.
 
-   ***********************
-   WINDOWS/GAMEEX SETUP:
+********************************************************************************/
 
-   
+#include <AccelStepper.h>
 
-
- ********************************************************************************/
+// for debugging
+const bool debug = false;
 
 // defines DRV8825/A4988 pins numbers
 const int dirPin = 2;
 const int stepPin = 3;
+const int mode0 = 6;
+const int mode1 = 7;
+const int mode2 = 8;
 const int enablePin = 9;
 
 // define switch pins
@@ -33,51 +39,63 @@ const int rotateButton = 10;
 const int v_Limiter = 11;
 const int h_Limiter = 12;
 
-// motor speed
-const int m_delay = 2000;
-const int maxSpeed = 800; // less is faster
-int currSpeed = m_delay;
+// motor settings
+const int maxSpeed = 1000;
+const int accelSpeed = 1000;
+const int endPos = 2000;
+
+// define mode and the pins
+AccelStepper stepper(1, stepPin, dirPin); // driver, step, dir
 
 // for current orientation
 bool isHorizontal;
 
 void setup()
 {
-  // initalize the serial port
+  // init serial
   Serial.begin(9600);
 
-  // disable motor control
-  digitalWrite(enablePin, HIGH);
+  // init AccelStepper
+  stepper.setEnablePin(enablePin);
+  stepper.setMaxSpeed(maxSpeed);
+  stepper.setAcceleration(accelSpeed);
 
-  // Sets the two pins as Outputs
-  pinMode(dirPin, OUTPUT);
-  pinMode(stepPin, OUTPUT);
-  digitalWrite(stepPin, LOW);
+  /***************************
+   *  MICROSTEPS: M0 M1 M2
+   *  FULL: LOW LOW LOW
+   *  HALF: HIGH LOW LOW
+   *  1/4: LOW HIGH LOW
+   *  1/8: HIGH HIGH LOW
+   *  1/16: LOW LOW HIGH
+   *  1/32: HIGH LOW HIGH
+   ***************************/
 
-  // limit switch
+  // microstep @ 1/16th
+  digitalWrite(mode0, LOW);
+  digitalWrite(mode1, LOW);
+  digitalWrite(mode2, HIGH);
+
+  // switches/buttons
   pinMode(h_Limiter, INPUT_PULLUP);
   pinMode(v_Limiter, INPUT_PULLUP);
-
-  // user button
   pinMode(rotateButton, INPUT_PULLUP);
 
-  // set monitor to home position (horizontal)
-  delay(1000);
+  // init monitor to home position (horizontal)
   Serial.println("setting to home position");
-  
   setHorizontal();
 }
 
 void loop()
 {
-  // check if reset button pressed
+  if (debug) onDebug();
+
+  // check if rotate button pressed
   if (digitalRead(rotateButton) == LOW)
   {
-    Serial.println("user button pressed");
-    if (isHorizontal) setVertical();
-    else if (!isHorizontal) setHorizontal();
+    Serial.println("Rotate Button Pressed...");
+    isHorizontal ? setVertical() : setHorizontal();
   }
-
+  
   // check if serial is available
   if (Serial.available() > 0)
   {
@@ -103,43 +121,44 @@ void loop()
 
 void setHorizontal()
 {
-  digitalWrite(enablePin, LOW);
-  digitalWrite(dirPin, HIGH); // counter-clockwise
+  stepper.disableOutputs();
+  stepper.moveTo(endPos);
 
   // if h_Limiter is not pressed
-  while (!digitalRead(h_Limiter))
+  while (!digitalRead(h_Limiter) && stepper.distanceToGo() > 0)
   {
-    rotateMonitor();
+    stepper.run();
   }
 
   isHorizontal = true;
-  currSpeed = m_delay;
-  digitalWrite(enablePin, HIGH);
+  stepper.enableOutputs();
+  stepper.setCurrentPosition(0);
 }
 
 void setVertical()
 {
-  digitalWrite(enablePin, LOW);
-  digitalWrite(dirPin, LOW); // clockwise
+  stepper.disableOutputs();
+  stepper.moveTo(-endPos);
 
   // if v_Limiter is not pressed
-  while (!digitalRead(v_Limiter))
+  while (!digitalRead(v_Limiter) && stepper.distanceToGo() < 0)
   {
-    rotateMonitor();
+    stepper.run();
   }
 
   isHorizontal = false;
-  currSpeed = m_delay;
-  digitalWrite(enablePin, HIGH);
+  stepper.enableOutputs();
+  stepper.setCurrentPosition(0);
 }
 
-void rotateMonitor()
+void onDebug()
 {
-  // use acceleration
-  if (currSpeed > maxSpeed) currSpeed -= 10;
+  // print to console if either limit switch is pressed
   
-  digitalWrite(stepPin, HIGH);
-  delayMicroseconds(currSpeed);
-  digitalWrite(stepPin, LOW);
-  delayMicroseconds(currSpeed);
+  if (digitalRead (h_Limiter)) {
+    Serial.println ("H Limit Pressed...");
+  }
+  if (digitalRead (v_Limiter)) {
+    Serial.println ("V Limit Pressed...");
+  }
 }
